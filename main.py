d71afc0bf0c53b872a08591b268153e576f966cf
Thomas <thomaslee9474@gmail.com>
2019-10-28 12:55:42 -0700

    [fix] simplify

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from aiohttp import web
 
+# Handlers
+
 
 async def foo_handle(request):
     return web.Response(text="I am foo")
@@ -9,3 +11,11 @@ async def bar_handle(request):
     output = await web.get('foo', foo_handle)
     if output:
         return web.Response(text="I am bar")
+
+# web app instance
+app = web.Application()
+app.router.add_get('/foo', foo_handle)
+app.router.add_get('/bar', bar_handle)
+
+# run app
+web.run_app(app)
