b045d6e066096c32baff687c9b4fb100aaed0023
Thomas <thomaslee9474@gmail.com>
2019-10-27 19:51:23 -0700

    [fix] foo route

--- server_1.py
+++ server_1.py
@@ -2,14 +2,9 @@ from req_handler import foo_handle
 from aiohttp import web
 from flask_api import status
 
-asyincio.run(main())
-server = Flask(__name__)
-
 # Coroutine 1
-@server.route("/foo")
-async def run():
-    await asyncio.sleep(5)
-    print("I am foo")
-    return status.HTTP_200_ok
+app = web.Application()
+app.add_routes([web.get('/foo', foo_handle)])
 
-    
+if __name__ == '__main__':
+    web.run_app(app)
