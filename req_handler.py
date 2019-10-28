5b85658104320e5458571e9b88c8563780a3b19f
Thomas <thomaslee9474@gmail.com>
2019-10-27 20:03:29 -0700

    [update] bar handler logic

--- req_handler.py
+++ req_handler.py
@@ -1,10 +1,9 @@
-import asyncio
+from aiohttp import web
 
 async def foo_handle(request):
         return web.Response(text="I am foo")
 
 async def bar_handle(request):
-    return web.Response(text="I am bar")
-
-
-
+    await output = web.get('foo', foo_handle)
+    if output:
+        return web.Response(text="I am bar")
